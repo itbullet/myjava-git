da52dfbedbfdc8ccf7b7f714e86d169e266f418e
Eduard Shapirov <eduard.shapirov@gmail.com>
2017-01-24 17:03:17 +0300

    My first Java program! Preparation for first homework on GeekBrains Java 1 course.

---
+++ src/ru/geekbrains/java/lesson1/MainClass.java
@@ -0,0 +1,27 @@
+package ru.geekbrains.java.lesson1;
+
+/**
+ * Created by esa on 24.01.2017.
+ */
+public class MainClass {
+    public static void main(String[] args) {
+        byte $a1 = 10;
+        short $a2 = 20;
+        int $a3 = -30;
+        long $a4 = 40L;
+        float $a5 = 50.6f;
+        double $a6 = -60.2121;
+        boolean $a7 = true;
+        char $a8 = 'X';
+
+        System.out.println($a1);
+        System.out.println($a2);
+        System.out.println($a3);
+        System.out.println($a4);
+        System.out.println($a5);
+        System.out.println($a6);
+        System.out.println($a7);
+        System.out.println($a8);
+
+    }
+}
