986d682c7f383b92a4b4379768e320ab389f6bcb
Eduard Shapirov <eduard.shapirov@gmail.com>
2017-01-31 23:17:16 +0300

    nothing (reverted from commit 24b246519c4fa48973a2fcee1d27f0999ba21410)

--- src/ru/geekbrains/java/lesson3/MainClass.java
+++ src/ru/geekbrains/java/lesson3/MainClass.java
@@ -1,62 +1,7 @@
 package ru.geekbrains.java.lesson3;
 
-import jdk.nashorn.internal.ir.WhileNode;
-
-import javax.sound.midi.Soundbank;
-import javax.sound.midi.SysexMessage;
-import java.util.Random;
-import java.util.Scanner;
-
 /**
  * Created by Eduard on 31.01.2017.
  */
 public class MainClass {
-    public static void main(String[] args) {
-        int $your_tries = 3;
-        gameRounds($your_tries);
-    }
-
-    public static void gameRounds(int $your_tries){
-        guessNumber($your_tries);
-        Scanner sc2 = new Scanner(System.in);
-        while(true) {
-            System.out.println("Повторить игру еще раз? y/n");
-            String $answer = sc2.nextLine();
-            if ($answer.equals("y")) {
-                guessNumber($your_tries);
-            } else {
-                System.out.println("Спасибо за игру");
-                break;
-            }
-        }
-    }
-
-    public static void guessNumber(int $try) {
-        Random $rand = new Random();
-        int $x = $rand.nextInt(10);
-        System.out.println("Угадайте какое число от 0 до 9 загадал компьютер. У вас " + $try + " попытки.");
-        System.out.println("Попытка " + $try + ". Введите число: ");
-        Scanner sc1 = new Scanner(System.in);
-        $try--;
-
-        while (true) {
-            if (sc1.hasNextInt()) {
-                int $guessNumber = sc1.nextInt();
-                if ($guessNumber == $x && $try > 0) {
-                    System.out.println("Поздравляем, вы угадали число!");
-                    break;
-                } else if ($guessNumber < $x && $try > 0) {
-                    System.out.println("К сожалению вы не угадали. Загаданное число больше. Попытка " + $try);
-                } else if ($guessNumber > $x && $try > 0) {
-                    System.out.println("К сожалению вы не угадали. Загаданное число меньше Попытка " + $try);
-                } else if ($try <= 0) {
-                    System.out.println("К сожалению, вы проиграли!");
-                    break;
-                }
-                $try--;
-            }
-            sc1.nextLine();
-        }
-
-    }
 }
