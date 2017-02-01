b990e985725275ed4fd4d3b63cfdfa29a956c8c8
Eduard Shapirov <eduard.shapirov@gmail.com>
2017-02-01 12:28:26 +0300

    changed amount of tries back to 3 from 4 I have added comments to 1st exercise lesson 3 Java1

--- src/ru/geekbrains/java/lesson3/MainClass.java
+++ src/ru/geekbrains/java/lesson3/MainClass.java
@@ -12,50 +12,58 @@ import java.util.Scanner;
  */
 public class MainClass {
     public static void main(String[] args) {
-        int $your_tries = 3;
-        gameRounds($your_tries);
+        int $your_tries = 3; //колличество попыток
+        //gameRounds($your_tries);
     }
 
-    public static void gameRounds(int $your_tries){
-        guessNumber($your_tries);
-        Scanner sc2 = new Scanner(System.in);
-        while(true) {
+    public static void guessWord() {
+        //создаем массив слов из которых компьютер будет загадывать слова
+        String[] words = {"apple", "orange", "lemon", "banana", "apricot", "avocado", "broccoli", "carrot", "cherry",
+                "garlic", "grape", "melon", "leak", "kiwi", "mango", "mushroom", "nut", "olive", "pea", "peanut",
+                "pear", "pepper", "pineapple", "pumpkin", "potato"};
+    }
+    //объявляем метод gameRounds, который будет отвечать за вызов игры, перезапуск раундов и окончание игры
+    public static void gameRounds(int $your_tries) {
+        guessNumber($your_tries); //вызываем игру
+        Scanner sc2 = new Scanner(System.in); //объявляем объект класса Scanner
+        //объявляем бесконечный цикл
+        while (true) {
             System.out.println("Повторить игру еще раз? y/n");
-            String $answer = sc2.nextLine();
-            if ($answer.equals("y")) {
+            String $answer = sc2.nextLine(); //считываем ответ пользователя из объекта sc2
+            if ($answer.equals("y")) { //если да то запускаем игру
                 guessNumber($your_tries);
-            } else {
+            } else { //если нет то Благодарим и прерываем цикл и выходим из программы
                 System.out.println("Спасибо за игру");
                 break;
             }
         }
     }
-
+    //объявляем метод guessNumber, который отвечает за игру в угадывание чисел
     public static void guessNumber(int $try) {
-        Random $rand = new Random();
-        int $x = $rand.nextInt(10);
+        Random $rand = new Random(); //созадем объект класса Random
+        int $x = $rand.nextInt(10); //вызываем метод nextInt с числом 10 для генерации чисел от 0 до 9
         System.out.println("Угадайте какое число от 0 до 9 загадал компьютер. У вас " + $try + " попытки.");
         System.out.println("Попытка " + $try + ". Введите число: ");
-        Scanner sc1 = new Scanner(System.in);
-        $try--;
+        Scanner sc1 = new Scanner(System.in); //объявляем новый объект класса Scanner
+        $try--; //уменьшаем колличество попыток
 
-        while (true) {
-            if (sc1.hasNextInt()) {
-                int $guessNumber = sc1.nextInt();
-                if ($guessNumber == $x && $try > 0) {
+        while (true) { //создаем бесконечный цикл
+            if (sc1.hasNextInt()) { //проверяем число ли ввели в Scanner
+                int $guessNumber = sc1.nextInt(); //если в сканере число то приравниваем его к переменной
+                if ($guessNumber == $x && $try > 0) { //если число угаданно вовдим сообщение и прекращаем цикл
                     System.out.println("Поздравляем, вы угадали число!");
                     break;
                 } else if ($guessNumber < $x && $try > 0) {
                     System.out.println("К сожалению вы не угадали. Загаданное число больше. Попытка " + $try);
                 } else if ($guessNumber > $x && $try > 0) {
                     System.out.println("К сожалению вы не угадали. Загаданное число меньше Попытка " + $try);
-                } else if ($try <= 0) {
+                } else if ($try <= 0) { // если колличество попыток израсходованно то прекращаем цикл
                     System.out.println("К сожалению, вы проиграли!");
                     break;
                 }
                 $try--;
             }
-            sc1.nextLine();
+            sc1.nextLine(); //считываем все что осталось в сканере - "очищаем его"
         }
 
     }
