a3924120b82bc631d67d5badef50e1231e0dfe49
Eduard Shapirov <eduard.shapirov@gmail.com>
2017-02-03 22:37:23 +0300

    Второй раз сделал commit что бы перестало светиться красным

--- src/ru/geekbrains/java/test/work1/Test1.java
+++ src/ru/geekbrains/java/test/work1/Test1.java
@@ -4,4 +4,22 @@ package ru.geekbrains.java.test.work1;
  * Created by Eduard on 03.02.2017.
  */
 public class Test1 {
+    public static void main(String[] args) {
+        print2DTable();
+    }
+
+    public static void print2DTable() {
+        for(int i = 0; i <= 10; i++) {
+            System.out.printf("%2d ", i);
+        }
+        System.out.println();
+        for (int i = 0; i <10; i++) {
+            System.out.printf("%2d ", i+1);
+            for (int j = 0; j < 10; j++) {
+                System.out.printf("%2s ", "*");
+            }
+            System.out.println();
+        }
+    }
+
 }
