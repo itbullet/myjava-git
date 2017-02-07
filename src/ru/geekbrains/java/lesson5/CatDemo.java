b7ae5fad1ccd886e8215810cb75d4942a94308e6
Eduard Shapirov <eduard.shapirov@gmail.com>
2017-02-07 17:04:02 +0300

    Начали изучать ООП создал класс CatDemo для работы с классом Cat

--- src/ru/geekbrains/java/lesson5/CatDemo.java
+++ src/ru/geekbrains/java/lesson5/CatDemo.java
@@ -1,7 +1,19 @@
 package ru.geekbrains.java.lesson5;
 
+import com.sun.org.apache.xpath.internal.SourceTree;
+
 /**
  * Created by esa on 07.02.2017.
  */
 public class CatDemo {
+    public static void main(String[] args) {
+        Cat cat1;
+        cat1 = new Cat("Barsik", "White", 4);
+        Cat cat2 = new Cat("Murzik", "Black", 6);
+        Cat cat3 = new Cat("Muska");
+        System.out.println("Cat1 name: " + cat1.getName() + " color: " + cat1.color + " age: " + cat1.getAge());
+        System.out.println("Cat2 name: " + cat2.getName() + " color: " + cat2.color + " age: " + cat2.getAge());
+        cat3.setAge(7);
+        System.out.println("Cat3 name: " + cat3.getName() + " color: " + cat3.color + " age: " + cat3.getAge());
+    }
 }
