4b40509b024d658c219dac2bc9994c81a142e798
Eduard Shapirov <eduard.shapirov@gmail.com>
2017-02-08 14:30:25 +0300

    Добавил комментарии перед сдачей ДЗ

--- src/ru/geekbrains/java/lesson5/Employee.java
+++ src/ru/geekbrains/java/lesson5/Employee.java
@@ -3,6 +3,9 @@ package ru.geekbrains.java.lesson5;
 /**
  * Created by esa on 07.02.2017.
  */
+/*
+Создаем класс Employee с полями Имя, Фамилия, email, телефон, зарплата, возраст.
+ */
 public class Employee {
     private String name;
     private String surname;
@@ -10,7 +13,9 @@ public class Employee {
     private String mobile_number;
     private int salary;
     private int age;
-
+/*
+Конструктор класса заполняет поля Имя, Фамилия, email, телефон, зарплата, возраст при создании объекта
+ */
     public Employee(String name, String surname, String email, String mobile_number, int salary, int age) {
         this.name = name;
         this.surname = surname;
@@ -19,7 +24,9 @@ public class Employee {
         this.salary = salary;
         this.age = age;
     }
-
+/*
+метод info выводит информацию об объекте в консоль
+ */
     public void info(){
         System.out.print("Name: " + name + " ");
         System.out.print("Surname: " + surname + " ");
@@ -28,7 +35,9 @@ public class Employee {
         System.out.print("Salary: " + salary + " ");
         System.out.println("Age: " + age + "\n");
     }
-
+/*
+Создаем геттер для получения поля age
+ */
     public int getAge() {
         return age;
     }
--- src/ru/geekbrains/java/lesson5/EmployeeDemo.java
+++ src/ru/geekbrains/java/lesson5/EmployeeDemo.java
@@ -5,13 +5,21 @@ package ru.geekbrains.java.lesson5;
  */
 public class EmployeeDemo {
     public static void main(String[] args) {
+        /*
+        Создаем массив из 5 сотрудников. Вначале объявляем массив объектов.
+         */
         Employee[] employeesArr = new Employee[5];
+        /*
+        Далее для каждой ячейки массива задаем объект
+         */
         employeesArr[0] = new Employee("Eduard","Shapirov","esa@unifeeder.com","+79112542748",2000,34);
         employeesArr[1] = new Employee("Anna","Bolkunova","abk@unifeeder.com","+79217698416",1000,30);
         employeesArr[2] = new Employee("Andrey","Evdokimov","aev@unifeeder.com","+79219644133",1800,54);
         employeesArr[3] = new Employee("Elena","Efremova","efr@unifeeder.com","+79217699962",1800,39);
         employeesArr[4] = new Employee("Benny","Hansen","bha@unifeeder.com","+79214121365",6000,47);
-
+        /*
+        С помощью цикла выводим информацию только о сотрудниках старше 40 лет
+         */
         for (int i=0; i < employeesArr.length; i++) {
             if(employeesArr[i].getAge() > 40) {
                 employeesArr[i].info();
