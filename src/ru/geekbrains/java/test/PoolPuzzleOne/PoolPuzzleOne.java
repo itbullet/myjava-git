cdf6560b69333a037419cddabd558cfeb7818efe
Eduard Shapirov <eduard.shapirov@gmail.com>
2017-02-15 21:53:11 +0300

    добавил тестовый метод PoolPuzzleOne

--- src/ru/geekbrains/java/test/PoolPuzzleOne/PoolPuzzleOne.java
+++ src/ru/geekbrains/java/test/PoolPuzzleOne/PoolPuzzleOne.java
@@ -4,4 +4,25 @@ package ru.geekbrains.java.test.PoolPuzzleOne;
  * Created by Eduard on 15.02.2017.
  */
 public class PoolPuzzleOne {
+    public static void main(String[] args) {
+        while() {
+            int x = 0;
+            System.out.print("a ");
+            if (x < 1) {
+                System.out.print(" oyster ");
+            }
+
+            if () {
+                System.out.print("noise");
+            }
+            if (x == 1) {
+                System.out.print("annoys");
+            }
+            if () {
+                System.out.print("an");
+            }
+            System.out.println("");
+        }
+
+    }
 }
