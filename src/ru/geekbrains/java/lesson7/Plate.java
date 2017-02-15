556db36cc12d5734dd8ab0a5455ac4d5d74eb17d
Eduard Shapirov <eduard.shapirov@gmail.com>
2017-02-15 16:49:29 +0300

    Добавил класс Тарелка

--- src/ru/geekbrains/java/lesson7/Plate.java
+++ src/ru/geekbrains/java/lesson7/Plate.java
@@ -4,4 +4,33 @@ package ru.geekbrains.java.lesson7;
  * Created by esa on 15.02.2017.
  */
 public class Plate {
+    private int food;
+
+    public Plate(int food) {
+        this.food = food;
+    }
+
+    public boolean decreaseFood(int n) {
+        if ((food - n) < 0) {
+            return false;
+        } else {
+            food -= n;
+            return true;
+        }
+    }
+
+    public void info() {
+        System.out.println("plate: " + food);
+    }
+
+    public void addFood(int n) {
+        food += n;
+    }
+
+    @Override
+    public String toString() {
+        return "Plate{" +
+                "food=" + food +
+                '}';
+    }
 }
