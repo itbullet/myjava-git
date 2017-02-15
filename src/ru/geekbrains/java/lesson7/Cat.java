442b7fd0020a4e2a7ab0de4a3ed80392f3812381
Eduard Shapirov <eduard.shapirov@gmail.com>
2017-02-15 16:49:38 +0300

    Добавил класс Кошка

--- src/ru/geekbrains/java/lesson7/Cat.java
+++ src/ru/geekbrains/java/lesson7/Cat.java
@@ -4,4 +4,26 @@ package ru.geekbrains.java.lesson7;
  * Created by esa on 15.02.2017.
  */
 public class Cat {
+    private String name;
+    private int appetite;
+    private boolean hungry;
+
+    public Cat(String name, int appetite) {
+        this.name = name;
+        this.appetite = appetite;
+        this.hungry = false;
+    }
+
+    public void eat(Plate plate1) {
+        hungry = plate1.decreaseFood(appetite);
+        if (hungry) {
+            System.out.println(name + " ест " + appetite + " из тарелки " + plate1.toString());
+        } else {
+            System.out.println(name + " не хватает еды в тарелке: " + plate1.toString());
+        }
+    }
+
+    public void info() {
+        System.out.println(name + " сытый: " + hungry);
+    }
 }
