638fa50e8207f8bc332acd69c7ffafe7efa6b361
Eduard Shapirov <eduard.shapirov@gmail.com>
2017-02-22 17:02:52 +0300

    Начал выполнять ДЗ к lesson1 Java 2

--- src/ru/geekbrains/java2/lesson1/MainClass.java
+++ src/ru/geekbrains/java2/lesson1/MainClass.java
@@ -4,4 +4,16 @@ package ru.geekbrains.java2.lesson1;
  * Created by esa on 22.02.2017.
  */
 public class MainClass {
+    public static void main(String[] args) {
+        Animal animal = new Animal("Animal");
+        Cat cat = new Cat("Barsik", "White");
+        animal.animalInfo();
+        cat.animalInfo();
+        cat.catInfo();
+
+        Animal a = new Animal();
+        Cat c = new Cat("Barsik", "White");
+        a.voice();
+        c.voice();
+    }
 }
