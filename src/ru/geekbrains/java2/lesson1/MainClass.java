78f3d579882b5c93e833d1472059de9f1b661b4a
Eduard Shapirov <eduard.shapirov@gmail.com>
2017-02-26 17:39:48 +0300

    удалил блок строк в классе MainClass

--- src/ru/geekbrains/java2/lesson1/MainClass.java
+++ src/ru/geekbrains/java2/lesson1/MainClass.java
@@ -8,18 +8,6 @@ import ru.geekbrains.java2.lesson1.course.*;
  */
 public class MainClass {
     public static void main(String[] args) {
-        /*Competitor[] competitors = {new Human("Bob"), new Cat("Barsik"), new Dog("Bobik"), new Dog("Sharik")};
-        Obstacle[] obstacles = {new Cross(300), new Wall(2), new Cross(800), new Water(5)};
-        for (Competitor c : competitors) {
-            for (Obstacle o : obstacles) {
-                o.doIt(c);
-                if (!c.isOnDistance()) break;
-            }
-        }
-        System.out.println("\n===== RESULTS =====\n");
-        for (Competitor c : competitors) {
-            c.result();
-        }*/
         Team team = new Team("Rocket", new Human("Escobar"), new Dog("Tarzan"), new Cat("Barsik"));
         Course course = new Course(new Cross(500), new Wall(1), new Water(1), new Cross(333));
         team.showInfo();
